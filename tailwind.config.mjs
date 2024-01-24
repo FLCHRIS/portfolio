eec53af2b232f93421ef2872f41176f0ffc5045a
Christian <filetelopezchristian@gmail.com>
2024-01-24 09:13:28 -0600

    Non-important changes :v:

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				Purple: '#7828C8',
+				Red: '#F31260',
+				Grey: '#D4D4D8',
+			},
+			fontFamily: {
+				sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+}
