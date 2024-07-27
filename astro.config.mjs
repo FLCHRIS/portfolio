c1789f75cecd9955f16b19b46c7f352cc169fcf9
Christian <filetelopezchristian@gmail.com>
2024-07-27 12:21:18 -0600

    New dependencie added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
-  markdown: {
-    shikiConfig: {
-      theme: 'one-dark-pro',
-      wrap: false
-    }
-  }
+  integrations: [
+    tailwind(),
+    react(),
+    expressiveCode({
+      themes: ["houston"],
+    }),
+    mdx(),
+  ],
 });
